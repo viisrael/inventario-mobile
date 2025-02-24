d2ba2e7b5ec1c68fd788306dc832097e4d8a6493
Vially Israel <viallyisrael+github@hotmail.com>
2025-02-24 15:30:18 -0300

    chore: creating SignupRequest

---
+++ InventarioMobile/Models/Request/SignupRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioMobile.Models.Request;
+public class SignupRequest
+{
+    public Guid UsuarioId { get; private set; }
+    public string Nome { get; private set; }
+    public string Email { get; private set; }
+    public string Senha { get; private set; }
+
+    public SignupRequest(string nome, string email, string senha)
+    {
+        UsuarioId = Guid.NewGuid();
+        Nome = nome;
+        Email = email;
+        Senha = senha;
+    }
+}
