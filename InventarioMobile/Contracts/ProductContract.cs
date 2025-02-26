fb8495b14f908d4268863ecda7aa3d18fbcb32d9
Vially Israel <viallyisrael+github@hotmail.com>
2025-02-25 22:36:46 -0300

    chore: creating product contract

---
+++ InventarioMobile/Contracts/ProductContract.cs
@@ -0,0 +1,31 @@
+﻿using Flunt.Validations;
+using InventarioMobile.Models.Request;
+
+namespace InventarioMobile.Contracts
+{
+    public class ProductContract: Contract<ProductRequest>
+    {
+        public ProductContract(ProductRequest request)
+        {
+            Requires()
+                .IsNotEmpty(request.ProductId, nameof(request.ProductId), "Id do produto é obrigatório");
+
+            Requires()
+                .IsNotNullOrEmpty(request.Descricao, nameof(request.Descricao), "Descrição do produto é obrigatório");
+
+            Requires()
+                .IsGreaterThan(request.Estoque, -1, nameof(request.Estoque), "Estoque não pode ser negativo");
+
+            Requires()
+                .IsNotNullOrEmpty(request.Barcode, nameof(request.Barcode), "Código de barras não pode ser vazio");
+
+            Requires()
+                .IsNotNullOrEmpty(request.UnidadeMedidada, nameof(request.UnidadeMedidada),
+                    "Informe uma unidade de medida");
+
+            Requires()
+                .IsGreaterThan(request.Preco, 0, nameof(request.Preco), "Preço deve ser maior que 0");
+
+        }
+    }
+}
